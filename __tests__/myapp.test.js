252bf08d7bfb8c15945d9a073d58b33ca7954e1d
faizal-carim <m.faizal.carim@gmail.com>
2022-03-01 21:58:33 +0200

    added a few more test cases

--- __tests__/myapp.test.js
+++ __tests__/myapp.test.js
@@ -10,10 +10,11 @@ beforeAll(async () => {
 describe("POST /register", () => {
     
     let bearer = "";
+    let wrong_bearer = "wrong_bearer";
     let userId = 1;
     let email = "faizal"+Date.now()+"@gmail.com";
 
-    test("It create a new user", async () => {
+    test("It creates a new user", async () => {
       const newUser = await request(app)
         .post("/register")
         .send({
@@ -25,6 +26,41 @@ describe("POST /register", () => {
       expect(newUser.statusCode).toBe(201);
     });
 
+    test("It tries to create a new user that already exists", async () => {
+        const newUser = await request(app)
+          .post("/register")
+          .send({
+              name:"Usman",
+              email:email,
+              password:"usman",
+              role:"ADMIN"
+          });
+        expect(newUser.statusCode).toBe(409);
+    });
+
+    test("It tries to create a new user without the required fields", async () => {
+        const newUser = await request(app)
+          .post("/register")
+          .send({
+              name:"Usman",
+              email:email
+          });
+        expect(newUser.statusCode).toBe(400);
+    });
+
+    test("It tries to create a new user that is neither ADMIN nor STAFF", async () => {
+        const newUser = await request(app)
+          .post("/register")
+          .send({
+              name:"Usman",
+              email:email,
+              password:"usman",
+              role:"USER"
+          });
+        expect(newUser.statusCode).toBe(409);
+    });
+
+
     test("It logs in a user", async () => {
         const user = await request(app)
           .post("/login")
@@ -35,6 +71,16 @@ describe("POST /register", () => {
         bearer = user.body.token;
         userId = user.body.userId;
         expect(user.statusCode).toBe(200);
+    });
+
+    test("It tries to log in a user with invalid credentials", async () => {
+        const user = await request(app)
+          .post("/login")
+          .send({
+              email:email,
+              password:"usman1"
+          });
+        expect(user.statusCode).toBe(400);
     });
 
     test("It creates a new schedule for the user", async () => {
@@ -49,6 +95,18 @@ describe("POST /register", () => {
         expect(user.statusCode).toBe(201);
     });
 
+    test("It tries to create a new schedule for the user that does not exist", async () => {
+        const user = await request(app)
+          .post("/newSchedule")
+          .set({ "Authorization": 'Bearer '+ bearer })
+          .send({
+            "userId":0,
+            "workDate":"2022-04-18",
+            "shiftLength":16
+          });
+        expect(user.statusCode).toBe(400);
+    });
+
     test("It updates a users name", async () => {
         const user = await request(app)
           .patch("/updateUser")
@@ -58,6 +116,17 @@ describe("POST /register", () => {
                 "name":"osman f"
           });
         expect(user.statusCode).toBe(200);
+    });
+
+    test("It updates a users name with invalid token", async () => {
+        const user = await request(app)
+          .patch("/updateUser")
+          .set({ "Authorization": 'Bearer '+ wrong_bearer })
+          .send({
+                "userId":userId,
+                "name":"osman f"
+          });
+        expect(user.statusCode).toBe(403);
     });
 
     test("It gets all the users schedules", async () => {
